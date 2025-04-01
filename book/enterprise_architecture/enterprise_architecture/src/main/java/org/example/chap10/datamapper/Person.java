6e20666c8dbc79e22f4b1e79125ae0b89a70e288
lightbell03 <bellight28@gmail.com>
2025-04-01 22:27:53 +0900

    annotation 적용

--- book/enterprise_architecture/enterprise_architecture/src/main/java/org/example/chap10/datamapper/Person.java
+++ book/enterprise_architecture/enterprise_architecture/src/main/java/org/example/chap10/datamapper/Person.java
@@ -1,6 +1,9 @@
 package org.example.chap10.datamapper;
 
+@Table(name = "person")
 public class Person {
+    @Id
+    private Long id;
     private String lastname;
     private String firstname;
     private int numberOfDependents;
@@ -9,5 +12,21 @@ public class Person {
         this.lastname = lastname;
         this.firstname = firstname;
         this.numberOfDependents = numberOfDependents;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public int getNumberOfDependents() {
+        return numberOfDependents;
     }
 }
