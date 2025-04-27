e25b246f1fd68f51eb5cd274e07fe5a15fbe3e2d
lightbell03 <bellight28@gmail.com>
2025-04-27 16:34:53 +0900

    필요한 domain class 추가

--- book/make_clean_architecture/code/buckpal/src/main/java/org/example/domain/Account.java
+++ book/make_clean_architecture/code/buckpal/src/main/java/org/example/domain/Account.java
@@ -1,4 +1,23 @@
 package org.example.domain;
 
 public class Account {
+    private AccountId id;
+    private Money baselineBalance;
+    private ActivityWindow activityWindow;
+
+    public Money calculateBalance() {
+        return null;
+    }
+
+    public boolean withdraw(Money money, AccountId targetAccountId) {
+        return false;
+    }
+
+    public boolean deposit() {
+        return false;
+    }
+
+    public static class AccountId {
+        private Long id;
+    }
 }
