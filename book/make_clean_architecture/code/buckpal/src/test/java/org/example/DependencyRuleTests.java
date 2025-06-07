6db8b9b2403a53d728f8f887e9cfeab2e63c2c10
lightbell03 <bellight28@gmail.com>
2025-06-07 15:16:54 +0900

    test 정리

--- book/make_clean_architecture/code/buckpal/src/test/java/org/example/DependencyRuleTests.java
+++ book/make_clean_architecture/code/buckpal/src/test/java/org/example/DependencyRuleTests.java
@@ -2,6 +2,7 @@ package org.example;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 
+import com.tngtech.archunit.core.importer.ClassFileImporter;
 import org.junit.jupiter.api.Test;
 
 public class DependencyRuleTests {
@@ -9,7 +10,12 @@ public class DependencyRuleTests {
 	@Test
 	void domainLayerDoesNotDependOnApplicationLayer() {
 		noClasses()
-			.that()
-			.resideInAnyPackage("buckpal.domain..")
+				.that()
+				.resideInAnyPackage("org.example.domain")
+				.should()
+				.dependOnClassesThat()
+				.resideInAnyPackage("org.example.application")
+				.check(new ClassFileImporter()
+						.importPackages("org.example"));
 	}
 }
